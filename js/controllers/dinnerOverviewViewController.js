cdf96f3194992cb8f21a97bbef88f688558b3e21
Bas Straathof <bas_straathof@msn.com>
2019-01-23 17:01:30 +0100

    Attach event handler to Print Recipe Button

--- js/controllers/dinnerOverviewViewController.js
+++ js/controllers/dinnerOverviewViewController.js
@@ -1,3 +1,5 @@
 var DinnerOverviewViewController = function(view, model, generalController) {
-  return 1;
+  view.printRecipeButton.click(() => {
+    generalController.showScreen("DINNER_PRINTOUT");
+  });
 }
