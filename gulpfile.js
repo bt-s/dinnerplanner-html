fd7756f9908e719cf257fbaea776a526bc5a7a51
Bas Straathof <bas_straathof@msn.com>
2019-01-28 23:25:52 +0100

    Minify SCSS and JS using Gulp
    All optimized files are stored in dist/
    NOTE: do not forget to run $ npm install

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,46 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const uglify = require('gulp-uglify-es').default;
+const rename = require('gulp-rename');
+
+const AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./styling/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./styling/css'));
+    .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe(csso())
+    .pipe(rename(function (path) {
+      path.extname = ".css.min";
+    }))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('js', function() {
+  return gulp.src('./js/**/*.js')
+    .pipe(uglify())
+    .pipe(rename(function (path) {
+      path.extname = ".js.min";
+    }))
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('default', ['sass', 'js']);
 
 gulp.task('sass:watch', function () {
   gulp.watch('./styling/sass/**/*.scss', ['sass']);
