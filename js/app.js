6b4aa8c3918445b66e4220a09f47da225bf2ca04
Bas Straathof <bas_straathof@msn.com>
2019-01-28 19:21:59 +0100

    Clean up app.js
    - Use 'let' and 'const' instead of 'var'
    - Remove SCREENS_Set because it seems to overcomplicate its purpose
    - Create storeCookies() and loadDataFromLocal() functions
    - Use window.addEventListener("unload"... instead of onunload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 $(function () {
   // We instantiate our model at the beginning
-  var model = new DinnerModel();
+  const model = new DinnerModel();
 
   // We instantiate our general controller
-  var generalController = new GeneralController();
+  const generalController = new GeneralController();
 
   // Create the instances of our view
-  var welcomeView = new WelcomeView($("#welcomeView"), model);
-  var sideBarView = new SideBarView($("#sideBarView"), model);
-  var dishSearchView = new DishSearchView($("#dishSearchView"), model);
-  var dishDetailView = new DishDetailView($("#dishDetailView"), model);
-  var dinnerOverviewView = new DinnerOverviewView($("#dinnerOverviewView"), model);
-  var titleBarView = new TitleBarView($("#titleBarView"), model);
-  var printView = new PrintView($("#printView"), model);
+  const welcomeView = new WelcomeView($("#welcomeView"), model);
+  const sideBarView = new SideBarView($("#sideBarView"), model);
+  const dishSearchView = new DishSearchView($("#dishSearchView"), model);
+  const dishDetailView = new DishDetailView($("#dishDetailView"), model);
+  const dinnerOverviewView = new DinnerOverviewView($("#dinnerOverviewView"), model);
+  const titleBarView = new TitleBarView($("#titleBarView"), model);
+  const printView = new PrintView($("#printView"), model);
 
   // Add all views to the general controller
   generalController
@@ -25,120 +25,121 @@ $(function () {
     .addView(printView);
 
   // Instantiate the controllers
-  var welcomeViewController =
+  const welcomeViewController =
     new WelcomeViewController(welcomeView, model, generalController);
-  var sideBarViewController =
+  const sideBarViewController =
     new SideBarViewController(sideBarView, model, generalController);
-  var dishSearchViewController =
+  const dishSearchViewController =
     new DishSearchViewController(dishSearchView, model, generalController);
-  var dishDetailsViewController =
+  const dishDetailsViewController =
     new DishDetailsViewController(dishDetailView, model, generalController);
-  var dinnerOverviewViewController =
+  const dinnerOverviewViewController =
     new DinnerOverviewViewController(dinnerOverviewView, model, generalController);
-  var titleBarViewController =
+  const titleBarViewController =
     new TitleBarViewController(titleBarView, model, generalController);
-  var printViewController =
+  const printViewController =
     new PrintViewController(printView, model, generalController);
-  var dishItemViewController =
+  const dishItemViewController =
     new DishItemViewController($("#searchedDishes"), model, generalController);
 
-  function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i].trim();
-      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
-  }
-
-  var SCREENS = [
+  const SCREENS = [
     "WELCOME",
     "SELECT_DISH",
     "DISH_DETAILS",
     "DINNER_OVERVIEW",
     "DINNER_PRINTOUT"
   ];
-  // this set is created from the SCREENS list
-  // key and value are the same
-  SCREENS_Set = new Set(SCREENS); // this should be global
-  SCREENS.forEach((item) => {
-    SCREENS_Set[item] = item;
-  });
 
   // Register && Initialize Odd Elements
   generalController.addOddElement("headLine", $("#headLine"));
   generalController.initOddElements();
+
   // Instantiate all screens
   generalController
-    .addScreen(SCREENS_Set.WELCOME, [welcomeView])
-    .addScreen(SCREENS_Set.SELECT_DISH, [sideBarView, dishSearchView])
-    .addScreen(SCREENS_Set.DISH_DETAILS, [sideBarView, dishDetailView])
-    .addScreen(SCREENS_Set.DINNER_OVERVIEW, [dinnerOverviewView, titleBarView])
-    .addScreen(SCREENS_Set.DINNER_PRINTOUT, [printView, titleBarView]);
+    .addScreen("WELCOME", [welcomeView])
+    .addScreen("SELECT_DISH", [sideBarView, dishSearchView])
+    .addScreen("DISH_DETAILS", [sideBarView, dishDetailView])
+    .addScreen("DINNER_OVERVIEW", [dinnerOverviewView, titleBarView])
+    .addScreen("DINNER_PRINTOUT", [printView, titleBarView]);
 
   // read stored data from local
-  var dataToStore = {
+  let dataToStore = {
     "currentScreen": "",
     "numberOfGuests": 0,
     "selectedDishes": [],
     "searchCondition": []
   };
 
-  if (document.cookie.length === 0) {
-    for (key in dataToStore) {
-      dataToStore[key] = localStorage.getItem(key);
+  let storeCookies = () => {
+    let getCookie = (cname) => {
+      let name = cname + "=";
+      let ca = document.cookie.split(';');
+      for (let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+      }
+      return "";
     }
-  } else {
-    for (key in dataToStore) {
-      dataToStore[key] = getCookie(key);
+
+    if (document.cookie.length === 0) {
+      for (key in dataToStore) {
+        dataToStore[key] = localStorage.getItem(key);
+      }
+    } else {
+      for (key in dataToStore) {
+        dataToStore[key] = getCookie(key);
+      }
     }
   }
-  // ---- Load data from local ----
-  if (dataToStore['selectedDishes'] !== null) {
-    dataToStore['selectedDishes'].split(',').forEach((id) => {
-      model.addDishToMenu(id);
+
+  function loadDataFromLocal() {
+    if (dataToStore['selectedDishes'] !== null) {
+      dataToStore['selectedDishes'].split(',').forEach((id) => {
+        model.addDishToMenu(id);
+      });
+      model.setNumberOfGuests(Number(dataToStore['numberOfGuests']));
+      dishSearchView.setSearchCondition(...dataToStore["searchCondition"].split(','));
+    }
+    // if the name is illegal, set to Welcome page
+    generalController.setCurrentScreen(
+      SCREENS.indexOf(dataToStore['currentScreen']) === -1 ?
+      "WELCOME" : dataToStore['currentScreen']
+    );
+    generalController.showScreen(generalController.getCurrentScreen());
+  }
+
+  storeCookies();
+  loadDataFromLocal();
+
+  // Save current screen name on machine
+  window.addEventListener("unload", function(event) {
+    let currentScreen = generalController.getCurrentScreen();
+    let numberOfGuests = model.getNumberOfGuests();
+    let searchCond = dishSearchView.getSearchCondition();
+
+    document.cookie = 'currentScreen=' + currentScreen;
+    document.cookie = "numberOfGuests=" + numberOfGuests;
+
+    let sl = [];
+    let slStr = '';
+    model.getSelectedDishes().forEach((dish) => {
+      sl.push(dish.id);
+      slStr += ',' + dish.id;
     });
-    model.setNumberOfGuests(Number(dataToStore['numberOfGuests']));
-    dishSearchView.setSearchCondition(...dataToStore["searchCondition"].split(','));
-  }
-  // if the name is illegal, set to Welcome page
-  generalController.setCurrentScreen(
-    SCREENS.indexOf(dataToStore['currentScreen']) === -1 ?
-    "WELCOME" : dataToStore['currentScreen']
-  );
-  generalController.showScreen(generalController.getCurrentScreen());
+    slStr = slStr.substr(1);
 
-
-  // save current screen name on machine
-  window.onunload = () => {
-    //Chrome doesn't support cookies for local .html files
-    document.cookie = 'currentScreen=' + generalController.getCurrentScreen();
-    document.cookie = "numberOfGuests=" + model.getNumberOfGuests();
-    var sl = [];
-    var slStr = '';
-    for (var i = 0; i < model.getSelectedDishes().length; i++) {
-      sl.push(model.getSelectedDishes()[i].id);
-      slStr += ',' + model.getSelectedDishes()[i].id;
-    }
-    slStr = slStr.substr(1); // delete the first ,
     document.cookie = "selectedDishes=" + sl;
-    document.cookie = "searchCondition=" + dishSearchView.getSearchCondition();
-    //This is used for Chrome
-    localStorage.setItem('currentScreen', generalController.getCurrentScreen());
-    localStorage.setItem('numberOfGuests', model.getNumberOfGuests() + '');
+    document.cookie = "searchCondition=" + searchCond;
+
+    // Chrome doesn't support cookies for local .html files, so the followiung
+    // is used for Chrome
+    localStorage.setItem('currentScreen', currentScreen);
+    localStorage.setItem('numberOfGuests', numberOfGuests + '');
     localStorage.setItem('selectedDishes', slStr);
-    var searchConStr = dishSearchView.getSearchCondition();
-    searchConStr = searchConStr[0] + ',' + searchConStr[1];
-    localStorage.setItem('searchCondition', searchConStr);
+    let searchCondStr = searchCond[0] + ',' + searchCond[1];
+    localStorage.setItem('searchCondition', searchCondStr);
     for (key in dataToStore) {
       dataToStore[key] = localStorage.getItem(key);
-      console.log(localStorage.getItem(key));
     }
-
-  };
-
-
-
-
+  });
 });
