d027c0d076ef34b72ae22d93597cd5699c820863
Bas Straathof <bas_straathof@msn.com>
2019-02-05 11:45:59 +0100

    Turn titleBarView into a class

--- js/view/titleBarView.js
+++ js/view/titleBarView.js
@@ -1,23 +1,35 @@
-let TitleBarView = function (container, model) {
-  let numberOfGuests = container.find("#numberOfGuests")
-    .html(model.getNumberOfGuests());
+class TitleBarView {
+  constructor(container, model) {
+    container.html(`
+      <h2>My Dinner:
+          <span id="numberOfGuests">${model.getNumberOfGuests()}</span>
+          people
+      </h2>
+      <button id="backButton" class="btn btn-orange btn-pointy">
+        <span>Go back and edit dinner</span>
+      </button>
+    `
+    );
 
-  this.backButton = container.find("#backButton");
-  this.printRecipeButton = container.find("#printRecipeButton");
+    this.backButton = container.find("#backButton");
+    this.printRecipeButton = container.find("#printRecipeButton");
 
-  this.update = (model, changeDetails) => {
-    if (changeDetails== "numberOfGuests") {
-      numberOfGuests.html(model.getNumberOfGuests());
+    this.update = (model, changeDetails) => {
+      let numberOfGuests = container.find("#numberOfGuests");
+
+      if (changeDetails== "numberOfGuests") {
+        numberOfGuests.html(model.getNumberOfGuests());
+      }
     }
+
+    model.addObserver(this.update);
+
+    this.hide = () => {
+      container.hide();
+    };
+
+    this.show = () => {
+      container.show();
+    };
   }
-
-  model.addObserver(this.update);
-
-  this.hide = () => {
-    container.hide();
-  };
-
-  this.show = () => {
-    container.show();
-  };
 }
