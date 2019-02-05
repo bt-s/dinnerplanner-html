67c1c57533b2db5e66cc9869f9ebd10349666959
Bas Straathof <bas_straathof@msn.com>
2019-02-05 11:55:57 +0100

    Turn dishSearchView into a class

--- js/view/dishSearchView.js
+++ js/view/dishSearchView.js
@@ -1,67 +1,85 @@
-let DishSearchView = function (container, model) {
-  this.dishTypeSelect = container.find("#dishTypeSelect").html("");
-  this.keywordInput = container.find("#keywordInput");
-  this.searchDishButton = container.find("#searchDishButton");
+class DishSearchView {
+  constructor(container, model) {
+    container.html(`
+      <div class="search-bar">
+        <h2>Find a dish</h2>
+        <div class="search-bar-container">
+          <input id="keywordInput" placeholder="Enter key word" />
+          <select id="dishTypeSelect">
+             <option value=''>All</option>
+          </select>
+          <button type="submit" id="searchDishButton" class="btn btn-orange">Search</button>
+        </div>
+      </div>
+      <div class="dish-items-container">
+        <div class="dish-items-wrapper" id="searchedDishes"></div>
+      </div>
+    </div>`
+    );
 
-  let titalizeWords = (words) => {
-    return words
-      .split(' ')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
-  };
+    this.dishTypeSelect = container.find("#dishTypeSelect");
+    this.keywordInput = container.find("#keywordInput");
+    this.searchDishButton = container.find("#searchDishButton");
 
-  let listDishesTypes = () => {
-    let option = $("<option/>").text("All").prop("value", "");
-    this.dishTypeSelect.append(option);
+    let listDishesTypes = () => {
+      let titalizeWords = (words) => {
+        return words
+          .split(' ')
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ');
+      };
 
-    model.getDishesTypes().forEach((type) => {
-      let option = $("<option/>").text(titalizeWords(type)).prop("value", type);
-      this.dishTypeSelect.append(option);
-    });
-  };
+      model.getDishesTypes().forEach((type) => {
+        let option = `<option value=${type}>${titalizeWords(type)}</option>`;
+        this.dishTypeSelect.append(option);
+      });
+    };
 
-  let showSearchedDishes = () => {
-    let searchedDishesContainer = container.find("#searchedDishes").html("");
-    model.getSearchedDishes().forEach((dish) => {
-      let dishItemView = new DishItemView(dish.id, model);
-      searchedDishesContainer.append(dishItemView.dishItem);
-    });
-  };
+    let showSearchedDishes = () => {
+      let searchedDishesContainer = container.find("#searchedDishes").html("");
 
+      model.getSearchedDishes().forEach((dish) => {
+        let dishItemView = new DishItemView(dish.id, model);
+        searchedDishesContainer.append(dishItemView.dishItem);
+      });
+    };
 
-  this.getSearchCondition = () => {
-    return [this.dishTypeSelect.prop("value"),
-      this.keywordInput.prop("value")
-    ];
-  };
+    this.getSearchCondition = () => {
+      return [
+        this.dishTypeSelect.prop("value"),
+        this.keywordInput.prop("value")
+      ];
+    };
 
-  this.setSearchCondition = (type, kwd) => {
-    for (let i = 0; i < this.dishTypeSelect.children().length; i++) {
-      if (this.dishTypeSelect.children()[i].value == type)
-        this.dishTypeSelect[0].selectedIndex = i;
-    }
+    this.setSearchCondition = (type, kwd) => {
+      for (let i = 0; i < this.dishTypeSelect.children().length; i++) {
+        if (this.dishTypeSelect.children()[i].value == type)
+          this.dishTypeSelect[0].selectedIndex = i;
+      }
 
-    this.keywordInput.prop("value", kwd);
-  };
+      this.keywordInput.prop("value", kwd);
+    };
 
-  this.update = (model, changeDetails) => {
-    if (changeDetails === "searchedDishes") {
-      showSearchedDishes();
-    }
-    if (changeDetails == "searchCondition") {
-      this.setSearchCondition(...model.getSearchCondition());
-    }
-  };
+    this.update = (model, changeDetails) => {
+      if (changeDetails === "searchedDishes") {
+        showSearchedDishes();
+      }
 
-  this.hide = () => {
-    container.hide();
-  };
+      if (changeDetails == "searchCondition") {
+        this.setSearchCondition(...model.getSearchCondition());
+      }
+    };
 
-  this.show = () => {
-    container.show();
-  };
+    this.hide = () => {
+      container.hide();
+    };
 
-  listDishesTypes();
-  model.operateSearch(...model.getSearchCondition());
-  model.addObserver(this.update);
+    this.show = () => {
+      container.show();
+    };
+
+    listDishesTypes();
+    model.operateSearch(...model.getSearchCondition());
+    model.addObserver(this.update);
+  }
 }
