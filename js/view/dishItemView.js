6168199e3f3a7ce1e11eec4b7b03fa31dacccbb9
Bas Straathof <bas_straathof@msn.com>
2019-02-06 15:27:32 +0100

    Fix image alt tag bug

--- js/view/dishItemView.js
+++ js/view/dishItemView.js
@@ -5,7 +5,7 @@ class DishItemView {
     this.dishItem = `
       <a id=${id}>
       <img src=${model.getImgBaseUrl() + dish["image"]}
-           alt={dish['title']}></img>
+           alt='${dish['title']}'/>
       <h3>${dish['title']}</h3>
       </a>`
   }
